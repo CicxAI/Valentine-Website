f3757f4af0eff9f401772e8c36b74a4ad8324565
Cicx <81908260+CicxAI@users.noreply.github.com>
2024-02-13 18:44:43 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-
-@app.route('/ander')
-def ander():
-    return render_template('ander.html')
-
 if __name__ == '__main__':
     app.run(debug=True, port=12916)
 
